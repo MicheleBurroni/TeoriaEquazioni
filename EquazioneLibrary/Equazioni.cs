539299d4bc854b670f9d2a42f492dce09ffe50c9
michiburro14@gmail.com <michiburro14@gmail.com>
2021-02-16 13:01:05 +0100

    EQUAZIONE DETERMINATA, IMPOSSIBILE E SECONDO GRADO

--- EquazioneLibrary/Equazioni.cs
+++ EquazioneLibrary/Equazioni.cs
@@ -4,5 +4,32 @@ namespace EquazioneLibrary
 {
     public class Equazioni
     {
+        public static bool IsDeterminated(double a)
+        {
+            if (a != 0)
+            {
+                return true;
+            } else
+                return false;
+        }
+        public static bool IsInconsisted(double a, double b)
+        {
+            if (a == 0 && b != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+        public static bool IsDegree2(double esponente, double a, )
+        {
+            if (esponente == 2 && a != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
     }
 }
--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioneLibrary;
 
 namespace EquazioniLibrary.Test
 {
@@ -6,8 +7,29 @@ namespace EquazioniLibrary.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EquazioneprimogradoDeterminata()
         {
+            double a = 11;
+            bool valoreAspettato = true;
+            bool valoreRitorno = Equazioni.IsDeterminated(a);
+            Assert.AreEqual(valoreAspettato, valoreRitorno);
         }
+        [TestMethod]
+        public void EquazioneDiPrimoGradoImpossibile()
+        {
+            double a = 8, b = 14;
+            bool valoreAspettato = true;
+            bool valoreRitorno = Equazioni.IsInconsisted(a, b);
+            Assert.AreEqual(valoreAspettato, valoreRitorno);
+        }
+        [TestMethod]
+        public void EquazioneDiSecondogrado()
+        {
+            double esponente = 2, a = 4;
+            bool valoreAspettato = true;
+            bool valoreRitorno = Equazioni.IsInconsisted(esponente, a);
+            Assert.AreEqual(valoreAspettato, valoreRitorno);
+        }
+
     }
 }
